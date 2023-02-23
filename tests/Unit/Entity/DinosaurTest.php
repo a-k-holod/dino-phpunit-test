db4c3ad84897fc69260c8b6f761a8d7dd5db4c9b
Angelika <angelika.holod@gmail.com>
2023-02-23 23:37:48 +0100

    Bad test.

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -2,14 +2,25 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
 
-  public function testItWorks(): void
+  public function testCanGetAndSetData(): void
   {
-    self::assertEquals(42,42);
+    $dino = new Dinosaur(
+      name: "Dinus",
+      genus: "Tyrannosaurus",
+      length: 10,
+      enclosure: 'Paddock A'
+    );
+
+    self::assertSame('Dinus', $dino->getName());
+    self::assertSame('Tyrannosaurus', $dino->getGenus());
+    self::assertSame(10, $dino->getLength());
+    self::assertSame('Paddock A', $dino->getEnclosure());
   }
 
 }
